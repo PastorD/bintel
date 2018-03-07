473e6df49e33ae25ce8d8ccabf94ed041efe5da8
Daniel Pastor <danpasmor@gmail.com>
2018-03-06 18:14:01 -0800

    python file to add lidar and world tfs together

---
+++ scripts/tf_optitrack_hokuyo.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+# ME132B - Lab 1B     Move the turtlebot along some basic closed shape
+#                     (so that it theoretically will end where it started)
+
+# A basic script to make the turtlebot move in the shape of a square
+# Press CTRL + C to stop.  To run:
+# On TurtleBot:
+# roslaunch turtlebot_bringup minimal.launch
+# On work station:
+# python goforward.py
+
+import rospy
+import tf
+from geometry_msgs.msg import Twist
+from kobuki_msgs.msg import SensorState
+import time
+import math
+import csv
+ 
+if __name__ == '__main__':
+    rospy.init_node('turtlebot_tf')
+    br_rgbd = tf.TransformBroadcaster()
+    br_lidar = tf.TransformBroadcaster()
+    rate = rospy.Rate(10.0)
+    while not rospy.is_shutdown():
+        #br_rgbd.sendTransform((0.0, 0.0, 0.18), (0.5, 0.5, 0.5, 0.5), rospy.Time.now(), "rgbd", "")
+        #br_rgbd.sendTransform((0.0, 0.0, 0.18), (0.5, 0.5, 0.5, 0.5), rospy.Time.now(), "camera_depth_optical_frame", "ME133_Turtlebot")
+        br_rgbd.sendTransform((0.06, 0.0, 0.23), (0, 0.0, 0.7071, 0.7071), rospy.Time.now(), "laser", "ME133_Turtlebot")
+        rate.sleep()
+        
