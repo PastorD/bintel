8d4e097fc34ad8ce85fdf029f0107bfd38172e4e
Daniel Pastor <danpasmor@gmail.com>
2019-04-08 09:50:56 -0700

    Add ros topics to main_controller

--- scripts/main_controller.py
+++ scripts/main_controller.py
@@ -1,32 +1,59 @@
-
-
 #!/usr/bin/env python
 
-
-import rospy
-import roslib
+# Python Common
 import argparse
-
-from  dynamical_model import DynamicalModel
 from yaml import load, dump
 
+# ROS 
+import rospy
+import roslib
+from geometry_msgs.msg import PoseStamped, Quaternion, Vector3, TwistStamped
+from mavros_msgs.msg import AttitudeTarget
+from visualization_msgs.msg import Marker
+
+# Project
+from  dynamical_model import DynamicalModel
+
+
 class Robot():
+    """
+    Class for a generic dynamical model of the form \dot{x} = f(x,u)
+    """
     def __init__(self):
-        pass
 
-def load_model(model_file_name):
-    with open(model_file_name, 'r') as stream:
-        model = load(stream)
-    return model
+        model_file_name = 'model_test.yaml'
 
-def controller_main():
+        self.init_ROS()        
+        self.model = self.load_model(model_file_name)
+        
 
-    model_file_name = 'model_test.yaml'
-    model = load_model(model_file_name)
-    
+    def load_model(self,model_file_name):
+        with open(model_file_name, 'r') as stream:
+            model = load(stream)
+        return model
+
+    def ROS_init(self,):
+        self.pub_sp = rospy.Publisher('/mavros/setpoint_raw/attitude', AttitudeTarget, queue_size=10)
+            
+        rospy.init_node('gotowaypoint', anonymous=True)
+        self.rate = rospy.Rate(60) # 10hz
+
+        self.controller = QUAD_position_controller('offboard_ctrl/gainsStart.yaml')
+            
+            # Subscribe to local position
+        self.local_pose = PoseStamped()
+        rospy.Subscriber('/mavros/local_position/pose', PoseStamped, self._read_position)
+        rospy.Subscriber('/mavros/local_position/velocity', TwistStamped, self._read_velocity)
+
+    def _read_velocity(self,data):
+		self.velocity = data
+
+    def _read_position(self,data):
+        self.local_pose = data
+
 
 if __name__ == '__main__':
     try:
-        controller_main()
+        drone = Robot()
     except rospy.ROSInterruptException:
         pass
