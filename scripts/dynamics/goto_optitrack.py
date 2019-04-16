91511202a79502512354c4baee01d9ef823ccb00
PastorD <dpastorm@caltech.edu>
2019-04-16 15:39:17 -0700

    Publish waypoint for position control

--- scripts/dynamics/goto_optitrack.py
+++ scripts/dynamics/goto_optitrack.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
+
 import rospy
 from std_msgs.msg import String
 from geometry_msgs.msg import PoseStamped
 from mavros_msgs.srv import SetMode
+from visualization_msgs.msg import Marker
 import roslib
 import tf
 import mavros
@@ -28,17 +30,38 @@ class gotooptitrack():
         self.waypoint.pose.position.x = 0
         self.waypoint.pose.position.y = 0
         self.waypoint.pose.position.z = 5
+
+        
+        self.marker_pub = rospy.Publisher('/waypoint_marker', Marker, queue_size=10)
+        self.waypoint_marker = Marker()
+        self.publish_waypoint()
+
        
         while not rospy.is_shutdown():
             result_mode = change_mode(0,"OFFBOARD")
-            #self.waypoint.header.stamp = self.local_pose.header.stamp
-
             self.pub_sp.publish(self.waypoint)
             rate.sleep()
     
     def _local_pose_cb(self,data):
         self.local_pose = data
-        
+    
+    def publish_waypoint(self):
+        # Set parameters and publish the waypoint as a Rviz marker
+        self.waypoint_marker.type = Marker.CUBE
+        self.waypoint_marker.header.frame_id = 'map'
+        self.waypoint_marker.scale.x = 0.1
+        self.waypoint_marker.scale.y = 0.1
+        self.waypoint_marker.scale.z = 0.1
+        self.waypoint_marker.color.r = 0.1
+        self.waypoint_marker.color.g = 1
+        self.waypoint_marker.color.b = 0
+        self.waypoint_marker.color.a = 1
+        self.waypoint_marker.pose = self.waypoint.pose
+
+        for i in range(3):
+            rospy.sleep(2)
+            self.marker_pub.publish(self.waypoint_marker)
+
 if __name__ == '__main__':
     try:
         gotoop = gotooptitrack()
