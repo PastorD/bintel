90c6453b83e61fca1b9765423878c28af9ee961c
Cafolkes <carl.axel.folkestad@gmail.com>
2019-05-06 21:38:53 -0700

    SIImplemented PD control and pos reset. Remaining parts are read/write messages to RL and calculate reward.

--- scripts/rl-lander/main_rl_lander.py
+++ scripts/rl-lander/main_rl_lander.py
@@ -7,20 +7,27 @@ import numpy as np
 
 # RL algorithm
 from replay_buffer import ReplayBuffer
-from learner import Learner
+#from learner import Learner
 
 # ROS
 import rospy
 from geometry_msgs.msg import PoseStamped, Quaternion, Vector3, TwistStamped
 from mavros_msgs.msg import AttitudeTarget, RCOut
+from mavros_msgs.srv import SetMode
+import mavros
+from mavros import command
 
+# Import classes from other files
+from rl_pos_controller import RLPosController
 
 class RL_lander():
     """
     """
 
     def __init__(self, n_ep, ep_length):
+        # Development environment variables
         self.is_simulation = True
+        self.is_test_mode = True  # Calculates all controls with PD (no RL)
 
         # Initialize current state variables
         self.p = namedtuple("p", "x y z")
@@ -32,7 +39,11 @@ class RL_lander():
 
         # Initialize desired trust and attitude variables for attitude commands
         self.p_init = namedtuple("p_init", "x y z")
+        self.p_final = namedtuple("p_final", "x y z")
+        self.v_d = namedtuple("v_d", "x y z")
         self.p_init.x, self.p_init.y, self.p_init.z = 0., 0., 1.5
+        self.p_final.x, self.p_final.y, self.p_final.z = 0., 0., 0.
+        self.v_d.x, self.v_d.y, self.v_d.z = 0., 0., 0.
         self.T_d = 0.0
         self.q_d = namedtuple("q_d", "w x y z")
         self.omg_d = namedtuple("omg_d", "x y z")
@@ -54,30 +65,68 @@ class RL_lander():
         self.main_loop_rate = 60
         self.init_ROS()
         self.msg = AttitudeTarget()
+        self.rl_pos_controller = RLPosController(is_simulation=self.is_simulation, is_test_mode=self.is_test_mode)
 
         # Initialize learner
-        sess = tf.Session()
+        #sess = tf.Session()
         state_dim, action_dim = 2, 1
         action_bound = 10.
         actor_lr, critic_lr = 0.0001, 0.001
         gamma, tau = 0.99, 0.001
         minibatch_size = 100
-        learner = Learner(sess, state_dim, action_dim, action_bound, actor_lr, critic_lr, tau, gamma, minibatch_size)
-        
-        
-        
+        #learner = Learner(sess, state_dim, action_dim, action_bound, actor_lr, critic_lr, tau, gamma, minibatch_size)
+
     def train_rl(self):
-        
         for ep in range(self.n_ep):
-            #Set T_RL using DDPG
-            self.run_episode() # Run episode with safety filter, update z_ep, zdot_ep, T_ep
+            self.reset_position()
+            self.run_episode()
 
     def run_episode(self):
+
+        for t in range(self.ep_length):
+            self.pd_attitude_ctrl()
+            self.create_attitude_msg(stamp=rospy.Time.now())
+            self.pub_attmsg.publish(self.msg)
+            self.rate.sleep()
+
+            if self.p.z < 0.05:
+                break
+
+    def pd_attitude_ctrl(self):
+        T_d, q_d = self.rl_pos_controller.get_ctrl(p=self.p, q=self.q, v=self.v, omg=self.omg,
+                                                   p_d=self.p_final, v_d=self.v_d, T_RL=self.T_RL)
+        self.T_d = T_d
+        self.q_d.x, self.q_d.y, self.q_d.z, self.q_d.w = q_d
+
+    def calc_reward(self):
         pass
 
+    def reset_position(self):
+        # Arm the drone
+        mavros.set_namespace()
+        command.arming(True)
+
+        rospy.wait_for_service('mavros/set_mode')
+        change_mode = rospy.ServiceProxy('mavros/set_mode', SetMode)
+
+        self.waypoint = PoseStamped()
+        self.waypoint.header.frame_id = 'map'
+        self.waypoint.pose.position.x = self.p_init.x
+        self.waypoint.pose.position.y = self.p_init.y
+        self.waypoint.pose.position.z = self.p_init.z
+
+
+        while not rospy.is_shutdown() and np.linalg.norm(
+                np.array([self.p_init.x - self.p.x,
+                          self.p_init.y - self.p.y,
+                          self.p_init.z - self.p.z])) > 0.05:
+            result_mode = change_mode(0, "OFFBOARD")
+            self.pub_posmsg.publish(self.waypoint)
+            self.rate.sleep()
+
     def init_ROS(self):
-        self.pub_sp = rospy.Publisher('/mavros/setpoint_raw/attitude', AttitudeTarget, queue_size=10)
-        self.pub_traj = rospy.Publisher('/mavros/setpoint_raw/trajectory', PoseStamped, queue_size=10)
+        self.pub_attmsg = rospy.Publisher('/mavros/setpoint_raw/attitude', AttitudeTarget, queue_size=10)
+        self.pub_posmsg = rospy.Publisher('mavros/setpoint_position/local', PoseStamped, queue_size=10)
 
         rospy.init_node('controller_bintel', anonymous=True)
 
@@ -92,7 +141,6 @@ class RL_lander():
             rospy.Subscriber('/mavros/local_position/velocity_body', TwistStamped, self._read_velocity)
         else:
             rospy.Subscriber('/mavros/local_position/velocity', TwistStamped, self._read_velocity)
-        rospy.Subscriber('/mavros/rc/out', RCOut, self._read_rc_out)
 
     def _read_position(self, data):
         self.p.x, self.p.y, self.p.z = data.pose.position.x, data.pose.position.y, data.pose.position.z
@@ -116,7 +164,7 @@ class RL_lander():
 
 if __name__ == '__main__':
     try:
-        lander = RL_lander(n_ep=200, ep_length=100) #TODO: Decide n_ep, ep_length
+        lander = RL_lander(n_ep=200, ep_length=2000) #TODO: Decide n_ep, ep_length
         lander.train_rl()
     except rospy.ROSInterruptException:
         pass
---
+++ scripts/rl-lander/rl_pos_controller.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env/ python
+import warnings
+from collections import namedtuple
+import numpy as np
+import math
+import scipy.io as sio
+import exceptions
+import tf
+
+
+class RLPosController:
+    def __init__(self, is_simulation, is_test_mode):
+        # Development environment variables
+        self.is_simulation = is_simulation
+        self.is_test_mode = is_test_mode
+        self.use_lqr_gains = False
+
+        # Model specific vairables
+        self.g = 9.81
+        if self.is_simulation:
+            self.hover_throttle = 0.56
+        else:
+            self.hover_throttle = 0.672
+        self.max_pitch_roll = math.pi / 3
+        if (self.use_lqr_gains):
+            self.K = sio.loadmat("scripts/lqr_gains_nominal.mat")["K"]
+        else:
+            lam = math.sqrt(1.2/3*9.81/.7)
+            lam_xy = math.sqrt(0.6/3*9.81/.7)
+            K_p_xy = 3**(lam_xy**2)  # 1**(lam_xy**2)
+            K_p_z  = 1*(lam**2)
+            K_d_xy = 2*lam_xy # 2math.sqrt(lam)*2*9.81/.7
+            K_d_z  = 2*lam # 2math.sqrt(lam)*2*9.81/.7 #
+            K_i_xy = 0.0 * (lam_xy**3)
+            K_i_z  = 0.0 * (lam**3)
+
+            self.K = np.zeros((3,6))
+            self.K[0, 0] = K_p_xy
+            self.K[0, 3] = K_d_xy
+            self.K[1, 1] = K_p_xy
+            self.K[1, 4] = K_d_xy
+            self.K[2, 2] = K_p_z
+            self.K[2, 5] = K_d_z
+
+    def get_ctrl(self, p, q, v, omg, p_d, v_d, yaw_d=0., T_RL=0.):
+        f_d = self.get_desired_force(p, q, v, omg, p_d, v_d, T_RL)
+        T_d = self.get_thrust(f_d)
+        q_d = self.get_attitude(f_d, yaw_d)
+
+        return T_d, q_d
+
+    def get_desired_force(self, p, q, v, omg, p_d, v_d, T_RL):
+        e_p = np.array([p.x - p_d.x, p.y - p_d.y, p.z - p_d.z])
+        e_v = np.array([v.x - v_d.x, v.y - v_d.y, v.z - v_d.z])
+        e = np.concatenate((e_p, e_v)).reshape(-1, 1)
+
+        f_d = namedtuple("f_d", "x y z")
+        f_d.x, f_d.y, f_d.z = (-np.dot(self.K, e)).flatten()*(self.hover_throttle/self.g)
+
+        if not self.is_test_mode:
+            f_d.z = T_RL #TODO: Check units of T_RL
+
+        # Project f_d into space of achievable force
+        try:
+            if math.isnan(self.max_pitch_roll):
+                s_oncone = float('inf')
+            elif self.max_pitch_roll <= 1e-2:
+                f_d.x = 0
+                f_d.y = 0
+                s_oncone = float('inf')
+            elif self.max_pitch_roll >= math.pi - 1e-2:
+                s_oncone = float('inf')
+            elif f_d.z / math.sqrt(f_d.x**2 + f_d.y**2) >= math.tan(self.max_pitch_roll):
+                s_oncone = float('inf')
+            else:
+                s_oncone = self.hover_throttle/(math.tan(self.max_pitch_roll)*math.sqrt(f_d.x**2 + f_d.y**2)-f_d.z)
+
+            a = f_d.x**2 + f_d.y**2 + f_d.z**2
+            b = 2 * self.hover_throttle * f_d.z
+            c = self.hover_throttle ** 2 - 1
+            s_onsphere = (-b + math.sqrt(b ** 2 - 4 * a * c)) / (2 * a)
+
+            s = min(1, s_onsphere, s_oncone)
+            f_d.x = f_d.x*s
+            f_d.y = f_d.y*s
+            f_d.z = f_d.z*s + self.hover_throttle
+        except exceptions.ZeroDivisionError:
+            if f_d.x**2 + f_d.y**2 + f_d.z**2 > 1e-4:
+                warnings.warn("Got an unexpected divide by zero exception - there's probably a bug")
+            f_d.x = f_d.x
+            f_d.y = f_d.y
+            f_d.z = f_d.z + self.hover_throttle
+
+        return f_d
+
+    def get_thrust(self, f_d):
+        return np.linalg.norm(np.array([f_d.x, f_d.y, f_d.z]))
+
+    def get_attitude(self, f_d, yaw_d):
+        q_worldToYawed = tf.transformations.quaternion_from_euler(0,0,yaw_d, axes='rxyz')
+        rotation_axis = tuple(np.cross((0,0,1), np.array([f_d.x, f_d.y, f_d.z])))
+        if np.allclose(rotation_axis, (0.0, 0.0, 0.0)):
+            unit_rotation_axis = rotation_axis
+        else:
+            unit_rotation_axis = tf.transformations.unit_vector(rotation_axis)
+        rotation_angle = math.asin(np.linalg.norm(rotation_axis))
+        q_yawedToBody = tf.transformations.quaternion_about_axis(rotation_angle, unit_rotation_axis)
+
+        q_d = tf.transformations.quaternion_multiply(q_worldToYawed, q_yawedToBody)
+
+        return q_d
