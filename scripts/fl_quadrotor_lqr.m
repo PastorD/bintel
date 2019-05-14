c45786d2dd34dc566fef953a2c36b533ef38f17d
Cafolkes <carl.axel.folkestad@gmail.com>
2019-05-14 10:59:34 -0700

    Updated FL LQR gains

--- scripts/fl_quadrotor_lqr.m
+++ scripts/fl_quadrotor_lqr.m
@@ -4,8 +4,8 @@
 a = [0 1; 0 0];
 b = [0; 1];
 
-A = blkdiag(a,a,a,a);
-B = blkdiag(b,b,b,b);
+A = blkdiag(a,a,a);
+B = blkdiag(b,b,b);
 Q = eye(size(A,1))
 R = eye(size(B,2))
 K = -lqr(A,B,Q,R)
--- scripts/nominal_gains.m
+++ scripts/nominal_gains.m
@@ -3,11 +3,12 @@
 
 g = 9.81;
 ht = 0.56;
+m = 1;%ht/g;
 
 A = [zeros(3) eye(3)
      zeros(3) zeros(3)];
 B = [zeros(3)
-     eye(3)]/g*ht;
+     1/m*eye(3)];
 Q = diag([1/(0.05)^2;1/(0.05)^2;1/(0.03)^2;ones(3,1)/(0.02)^2])
 R = eye(3)/(1.0)^2
 K = lqr(A,B,Q,R)
