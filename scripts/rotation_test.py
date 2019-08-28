ea9fb57fbb1fa86ed260f2880dae827fa671a444
Daniel Pastor <dpastorm@caltech.edu>
2019-08-28 15:06:22 -0700

    more cleaning. TODO: check all scripts are working

--- scripts/rotation_test.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-import rospy
-from scipy.spatial.transform import Rotation as R
-
-
-r = R.from_euler('zyx',[[10,10,10]],degrees=True)
-
-print(r.as_quat())
---
+++ tmux/tmux_bintel.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+SESSION="runBintel"
+
+############################## PANES #########################################
+
+# Split Panes
+tmux -2 new-session -d -s $SESSION
+tmux split-window -v -p 66  
+tmux select-pane -t 1
+tmux split-window -v  -p 50
+tmux select-pane -t 0
+tmux split-window -h -p 50
+tmux select-pane -t 2
+tmux split-window -h -p 50
+tmux select-pane -t 4
+tmux split-window -h -p 50
+
+
+#######################################################################################
+############################# Drone  ##################################################
+#######################################################################################
+tmux send-keys -t 0 "sudo ntpq -p" C-m
+export DRONE=bintel
+tmux send-keys -t 0 "./increase_rates.sh" C-m
+sleep 2
+
+
+############################# Mavros ##############################################
+tmux send-keys -t 1 "roslaunch bintel_ros mavros_optitrack.launch" C-m
+
+sleep 2
+
+############################# Optitrack ###########################################
+tmux send-keys -t 0 "roslaunch bintel_ros vrpn_optitrack_positioning.launch" C-m
+
+sleep 2
+
+############################# Optitrack ###########################################
+tmux send-keys -t 0 "rosbag record --split --size=1900 /mavros/vision_pose/pose /mavros/imu/data /mavros/rc/out /mavros/rc/in /mavros/local_position/pose /mavros/local_position/velocity /tf /
+" C-m
+
+sleep 2
+
+
+#######################################################################################
+############################# Ground Station ##########################################
+#######################################################################################
+tmux send-keys -t 0 "sudo ntpq -p" C-m
+export DRONE=bintel
+
+
+############################# QGroundControl ######################################
+# use this file to open Qgroundcontrol
+tmux send-keys -t 0 "~/Downloads/QGroundControl.AppImage" C-m
+
+
+
+############################# QGroundControl ######################################
+
+# use this file to show the cast environment
+tmux send-keys -t 0 "roslaunch bintel_ros cast_models.launch robot_urdf:='urdf/inteldrone_down.urdf'" C-m
+
+
+
+
+########################### Terminate #############################################
+
+tmux select-pane -t 9
+
+tmux -2 attach-session -t $SESSION
